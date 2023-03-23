91f03ef64649f36d9a6db08187357b77e2caa1ae
MagnusNilssonn <mankan.nilsson@gmail.com>
2023-03-23 14:29:14 +0000

    correct typing error

--- codestar/settings.py
+++ codestar/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'clousinary_storage',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'blog',
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [os-path.join(BASE_DIR, 'static' )]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'clouinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 # Default primary key field type
